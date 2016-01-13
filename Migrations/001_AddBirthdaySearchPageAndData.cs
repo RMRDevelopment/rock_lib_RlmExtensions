110710eb14490f9ba7917cbd7fe79cb5180166d2
Kyle Craviotto <im.kyle@gmail.com>
2016-01-13 12:02:43 -0800

    fix typo

--- Migrations/001_AddBirthdaySearchPageAndData.cs
+++ Migrations/001_AddBirthdaySearchPageAndData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace com.reallifeministries.RockExtensions.Workflow.Migrations
+namespace com.reallifeministries.RockExtensions.Migrations
 {
     using Rock.Plugin;
 
