de90a18787ab6a6232016e237c26e73ed14f1f29
Kyle Craviotto <im.kyle@gmail.com>
2016-08-30 09:43:45 -0700

    add in copyright

--- Reporting/Group/ScheduleSelect.cs
+++ Reporting/Group/ScheduleSelect.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Web.UI.WebControls;
 using Rock.Data;
 using Rock.Model;
-using Rock.Web.Cache;
-using Rock.Web.UI.Controls;
 using Rock.Reporting;
-using Rock;
 
 namespace com.reallifeministries.RockExtensions.Reporting.Group
 {
